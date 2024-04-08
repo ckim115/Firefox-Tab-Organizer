7a87f7814b6977e4886be90ae23208cb9f242465
ckim115 <christina.hayoung@gmail.com>
2024-04-08 12:23:46 -0700

    add delete and bookmark functionalities

--- tabs.js
+++ tabs.js
@@ -1,1 +1,123 @@
+function getCurrentWindowTabs() {
+    console.log("Getting current window tabs");
+    return browser.tabs.query({ currentWindow: true });
+}
+
+function listTabs() {
+    getCurrentWindowTabs().then((tabs) => {
+        const tabsList = document.getElementById('tabs-list');
+        const currentTabs = document.createDocumentFragment();
+        let counter = 0;
+        tabsList.textContent = '';
+        for (const tab of tabs) {
+            const tabLink = document.createElement("a"); 
+                
+            tabLink.textContent = tab.title
+          
+            tabLink.setAttribute("href", "#"); 
+            tabLink.setAttribute("id", tab.id); 
+            tabLink.classList.add("bookmark-remove-chosen");
+            currentTabs.appendChild(tabLink);
+            currentTabs.appendChild(document.createElement("br"));
+            counter += 1;
+        }
+        tabsList.appendChild(currentTabs); // adds tabsList to tabs-list
+    });
+}
+
+//not working
+function makeAlert() { //notify user when over 14 tabs are present
+    console.log(getCurrentWindowTabs().length);
+    if(getCurrentWindowTabs().length >= 14) {
+        const notification = new Notification("You have a lot of tabs open!", {
+            body: "You currently have 14 or more tabs. Try decreasing the amount!",
+            icon: "icons/alert-icon-1562.png"
+        });
+        document.addEventListener("visibilitychange", () => { //when notification becomes visible, remove
+            if (document.visibilityState === "visible") {
+              notification.close();
+            }
+        });
+    }
+}
+
+//bro fix the sort lmao
+function organizeByTime(oldestFirst) {
+    console.log("Organizing...")
+    getCurrentWindowTabs().then((tabs) => {
+        for (var i = 0; i < tabs.length-1; i++) { //make more efficient later
+            var curTab = tabs[i];
+            for(var j = i+1; j < tabs.length; j++) {
+                if((!oldestFirst && curTab.lastAccessed > tabs[i+1].lastAccessed) || (oldestFirst && curTab.lastAccessed < tabs[i+1].lastAccessed)) {
+                    curTab = tabs[j];
+                }
+            }
+            browser.tabs.move([curTab.id], { index: i });
+        }
+    });
+}
+
+//works; finished
+function removeDuplicates() { //loop through the tabs; if there is duplicate(s), remove duplicates
+    console.log("Removing duplicates...")
+    getCurrentWindowTabs().then((tabs) => {
+        for (var i = 0; i < tabs.length-1; i++) {
+            console.log(tabs[i].url);
+            for(var j = i+1; j < tabs.length; j++) {
+                if(tabs[i].url === tabs[j].url) {
+                    browser.tabs.remove(tabs[j].id);
+                    console.log("Tab removed");
+                }
+            }
+        }
+    });
+}
+
+// Need to check
+// TODO: make it that users can input "old" amount
+function removeOld() { //loop through tabs; if a tab has been last acessed >= week ago, remove
+    console.log("Removing old...")
+    getCurrentWindowTabs().then((tabs) => {
+        for (const tab of tabs) {
+            if(tab.lastAccessed >= 604800000) { //in milliseconds
+                browser.tabs.remove(tab.id);
+            }
+        }
+    });
+}
+
+//works; finished
+function bookmarkAndRemove(tab) {
+    getCurrentWindowTabs().then((tabs) => {
+        for (const t of tabs) {
+            if(t.id == tab.id) { //find the tab with the same id and bookmark/remove it
+                browser.bookmarks.create({ title: t.title, url: t.url });
+                browser.tabs.remove(t.id);
+            }
+        }
+        tab.remove();
+    });
+}
+
 document.addEventListener("DOMContentLoaded", listTabs);
+document.addEventListener("DOMContentLoaded", makeAlert);
+
+document.addEventListener("click", (e) => {
+    if (e.target.id === "organize-time-oldest") {
+        organizeByTime(true);
+        console.log("organized");
+    } else if (e.target.id === "organize-time-newest") {
+        organizeByTime(false);
+        console.log("organized");
+    } else if (e.target.id === "remove-duplicates") {
+        removeDuplicates();
+    } else if (e.target.id === "remove-old") {
+        removeOld();
+        console.log("old removed");
+    } else {
+        bookmarkAndRemove(e.target);
+        console.log("bookmarked and removed");
+    }
+    browser.runtime.reload(); //refresh extension to update what is on its menu
+});
+  
