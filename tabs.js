86ebaca943b7a673418d7cb6197eadcdf6fa9350
ckim115 <christina.hayoung@gmail.com>
2024-04-26 20:09:33 -0700

    fix sort

--- tabs.js
+++ tabs.js
@@ -9,7 +9,6 @@ function listTabs() {
     getCurrentWindowTabs().then((tabs) => {
         const tabsList = document.getElementById('tabs-list');
         const currentTabs = document.createDocumentFragment();
-        let counter = 0;
         tabsList.textContent = '';
 
         for (const tab of tabs) {
@@ -22,7 +21,6 @@ function listTabs() {
             tabLink.classList.add("bookmark-remove-chosen");
             currentTabs.appendChild(tabLink);
             currentTabs.appendChild(document.createElement("br"));
-            counter += 1;
         }
         tabsList.appendChild(currentTabs); // adds tabsList to tabs-list
     });
@@ -31,8 +29,11 @@ function listTabs() {
 // Creates a warning on the extension display when over a certain number of tabs are created.
 function warnUser() {
     const limitval = document.getElementById("num-tabs").value;
+    console.log("warning val = " + limitval);
     const warning = document.getElementById("for-notif");
     getCurrentWindowTabs().then((tabs) => {
+        console.log(tabs.length);
+        console.log(limitval);
         if(tabs.length >= limitval) {
             warning.style.display = "block";
             warning.textContent = "You currently have " + limitval + " or more tabs!";
@@ -48,23 +49,28 @@ function ignoreWarning() {
     warning.style.display = "None";
 }
 
-//bro fix the sort lmao
+//Moves all the oldest tabs to the back/front depending on the user's choice
 function organizeByTime(oldestFirst) {
-    console.log("Organizing...")
+    console.log("Organizing...");
+    const weeks = document.getElementById("num-organize").value;
+    const ms = weeksToMilliseconds(weeks);
+    const msNow = Date.now();
+    console.log(ms);
+    console.log(msNow);
     getCurrentWindowTabs().then((tabs) => {
-        for (var i = 0; i < tabs.length-1; i++) { //make more efficient later
-            var curTab = tabs[i];
-
-            for(var j = i+1; j < tabs.length; j++) {
-                if((!oldestFirst && curTab.lastAccessed > tabs[i+1].lastAccessed) || (oldestFirst && curTab.lastAccessed < tabs[i+1].lastAccessed)) {
-                    curTab = tabs[j];
-                }
+        for(var i = 0; i < tabs.length; i++) {
+            if(!oldestFirst && msNow-tabs[i].lastAccessed >= ms) {
+                console.log("moving (oldest last)");
+                console.log(tabs[i].lastAccessed);
+                browser.tabs.move(tabs[i].id, { index: -1 });
             }
-
-            browser.tabs.move([curTab.id], { index: i });
+            if(oldestFirst && msNow-tabs[i].lastAccessed >= ms) {
+                console.log("moving (oldest first)");
+                console.log(tabs[i].lastAccessed);
+                browser.tabs.move(tabs[i].id, { index: 0 });
+            }
         }
     });
-    browser.runtime.reload();
 }
 
 // Finds and removes tabs with duplicate urls
@@ -93,10 +99,12 @@ function removeDuplicates() { //loop through the tabs; if there is duplicate(s),
 function removeOld() { //loop through tabs; if a tab has been last acessed >= week ago, remove
     console.log("Removing old...")
     const weeks = document.getElementById("num-weeks").value
+    const ms = weeksToMilliseconds(weeks);
+    console.log(typeof(ms) + " " + ms);
     var numDel = 0;
     getCurrentWindowTabs().then((tabs) => {
         for (const tab of tabs) {
-            if(Date.now() - tab.lastAccessed >= 604800000*weeks) { //in milliseconds since the epoch
+            if(Date.now() - tab.lastAccessed >= ms) { //in milliseconds since the epoch
                 browser.tabs.remove(tab.id);
                 numDel++;
             }
@@ -123,6 +131,16 @@ function bookmarkAndRemove(tab) {
 // // Gets the saved local storage information for number of weeks before a tab is considered 'old' and
 // // the reccomended number of tabs to initialize the extension when opening it
 function updateInputs() {
+    const org = browser.storage.local.get("min-organize");
+    org.then((result) => {
+        const results = Object.values(result);
+        for(const r of results) {
+            const rInt = parseInt(r);
+            if(rInt !== "NaN") {
+                document.getElementById("num-organize").value = rInt;
+            }
+        }
+    });
     const weeks = browser.storage.local.get("min-weeks");
     weeks.then((result) => {
         //should be only one
@@ -130,7 +148,7 @@ function updateInputs() {
         for(const r of results) {
             const rInt = parseInt(r);
             if(rInt !== "NaN") {
-                document.getElementById("num-weeks").value = parseInt(r);
+                document.getElementById("num-weeks").value = rInt;
             }
         }
     });
@@ -139,26 +157,37 @@ function updateInputs() {
         const results = Object.values(result);
         for(const r of results) {
             const rInt = parseInt(r);
+            console.log("during updates, max tabs: " + rInt);
             if(rInt !== "NaN") {
-                document.getElementById("num-tabs").value = parseInt(r);
+                console.log("updating...");
+                document.getElementById("num-tabs").value = rInt;
+                console.log("new val = " + document.getElementById("num-tabs").value);
+                warnUser();
             }
         }
     });
 }
 
+//returns milliseconds in a number of weeks
+function weeksToMilliseconds(n) {
+    //for presentation, change to secondToMilliseconds
+    //const ans = n*1000;
+    const ans = n*604800000;
+    return ans;
+}
+
 // Runs when extension loads
 document.addEventListener("DOMContentLoaded", listTabs);
 document.addEventListener("DOMContentLoaded", updateInputs);
-document.addEventListener("DOMContentLoaded", warnUser);
 
-
+//actions when clicking on parts of the extension
 document.addEventListener("click", (e) => {
     if (e.target.id === "organize-time-oldest") {
-        organizeByTime(true);
         console.log("Organizing by time (oldest)...")
+        organizeByTime(true);
     } else if (e.target.id === "organize-time-newest") {
-        organizeByTime(false);
         console.log("Organizing by time (newest)...")
+        organizeByTime(false);
     } else if (e.target.id === "remove-duplicates") {
         console.log("Removing duplicates...")
         removeDuplicates();
@@ -167,8 +196,10 @@ document.addEventListener("click", (e) => {
         removeOld(); //get input from num-weeks and pass in
     }else if (e.target.id === "for-notif") {
         console.log("Ignoring...");
-        //something here to make it dissappear
         ignoreWarning();
+    } else if (e.target.id === "num-organize") {
+        //saves in local storage number of weeks to organize
+        browser.storage.local.set({ ["min-organize"] : document.getElementById("num-organize").value });
     } else if (e.target.id === "num-weeks") {
         //saves in local storage the number of weeks
         browser.storage.local.set({ ["min-weeks"] : document.getElementById("num-weeks").value });
